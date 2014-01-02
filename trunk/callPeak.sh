899aa86af91ec7978d5f8fe4652e67430f99a4fd
yrjie0@gmail.com <yrjie0@gmail.com@d0e8cd11-8786-f6f3-35eb-0028881579cd>
2014-01-02 14:06:17 +0000

    added getProb file

--- trunk/callPeak.sh
+++ trunk/callPeak.sh
@@ -12,7 +12,7 @@ prefix=$3
 plusBed=$prefix'Plus.bed'
 minusBed=$prefix'Minus.bed'
 peakBed=$prefix'Peak.bed'
-top=200000
-sort -k4 -gr $plusSig |head -n $top >$plusBed
-sort -k4 -gr $minusSig |head -n $top >$minusBed
-cat $plusBed $minusBed |sortBed |mergeBed -nms >$peakBed
+top=700000
+sort -k5 -gr $plusSig |head -n $top >$plusBed
+sort -k5 -gr $minusSig |head -n $top >$minusBed
+cat $plusBed $minusBed |sortBed |mergeBed -scores mean >$peakBed

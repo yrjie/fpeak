9ec2988e0644fa4767d318441d83cc496a4725a5
yrjie0@gmail.com <yrjie0@gmail.com@d0e8cd11-8786-f6f3-35eb-0028881579cd>
2014-04-03 14:25:39 +0000

    init of Fseq

---
+++ trunk/src/io/DensityWriter.java
@@ -0,0 +1,11 @@
+package io;
+
+import java.io.IOException;
+
+public interface DensityWriter {
+  public void writeDensity(float[] batch, int start, int length) throws IOException;
+  
+  public void close() throws IOException;
+  
+  public void setThreshold(float threshold);
+}
